d997eb3e60ad4ba6a73ef3ff14be5ebd45506b89
Akanksha Chauhan <akankaku1998@gmail.com>
2023-11-04 11:29:09 +0530

    remove row version

--- DatingApp/Models/BaseModels/EntityBaseModel.cs
+++ DatingApp/Models/BaseModels/EntityBaseModel.cs
@@ -6,10 +6,6 @@ namespace DatingApp.Models.BaseModels
     {
         public int Id { get; set; }
         public DateTimeOffset Timestamp { get; set; } = DateTimeOffset.Now;
-
-        [Timestamp]
-        public byte[] RowVersion { get; set; }
-
         public DateTimeOffset UpdatedTimestamp { get; set; }
         public DateTimeOffset DeleteTimestamp { get; set; }
         public bool IsDeleted { get; set; } = false;
